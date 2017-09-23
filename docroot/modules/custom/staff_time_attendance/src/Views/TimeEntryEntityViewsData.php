a7dda2ebbc23d9608f52cc803e1fe48a9b1135a0
Cristian Romanescu <cristian@romanescu.ro>
2017-09-23 22:26:52 +0300

    Remove debug statements

--- docroot/modules/custom/staff_time_attendance/src/Views/TimeEntryEntityViewsData.php
+++ docroot/modules/custom/staff_time_attendance/src/Views/TimeEntryEntityViewsData.php
@@ -8,8 +8,6 @@ class TimeEntryEntityViewsData extends EntityViewsData {
 
   public function getViewsData() {
     $structure = parent::getViewsData();
-    var_dump(array_keys($structure['staff_time_entry']['created']['sort']));
-    var_dump($structure['staff_time_entry']['created']['sort']);
     //$structure['staff_time_entry']['created']['sort'] = ['id' => 'numeric'];
     return $structure;
   }
